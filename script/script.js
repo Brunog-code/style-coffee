b461a2c980add66434882ad6b782fff8fdf35b18
Brunog-code <brunogon.silva@hotmail.com>
2025-01-22 10:14:58 -0300

    animacao imgs

--- script/script.js
+++ script/script.js
@@ -21,6 +21,28 @@ document.querySelector('#arrow').addEventListener('click', function() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
 
-//escutar o evento scroll
+//escutar o evento scroll para mostrar o botao
 window.addEventListener('scroll', buttomTop);
+
+//ANIMACAO IMGS
+const elementos = document.querySelectorAll('[data-anima]');
+const animacaoClass = 'animacao';
+
+function animaScroll(){
+    const topPagina = window.scrollY+((window.innerHeight*3)/4);
+    elementos.forEach(function(elemento){
+        const elementopo = elemento.getBoundingClientRect().top + window.scrollY;
+        if (topPagina > elementopo){
+            elemento.classList.add(animacaoClass);
+        }else{
+            elemento.classList.remove(animacaoClass);
+        }
+    })
+}
+
+if (elementos.length){
+    window.addEventListener('scroll', function(){
+        animaScroll();
+    })
+}
 
