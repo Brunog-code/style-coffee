02d283dceda21f59ccf8df917c508393944b63b2
Brunog-code <brunogon.silva@hotmail.com>
2025-01-21 21:30:56 -0300

    atualizei botao scroll

--- script/script.js
+++ script/script.js
@@ -1,3 +1,26 @@
+//Funcao para verificar altura da pagina
+function buttomTop(){
+    const arrow = document.getElementById('arrow')
+    const bkArrow = document.getElementById('bk-arrow')
+
+    const scrollPosition = window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercentage = (scrollPosition / pageHeight) * 100;
+
+    if (scrollPercentage > 50){
+        arrow.style.display = 'block';
+        bkArrow.style.display = 'block';
+    }else{
+        arrow.style.display = 'none';
+        bkArrow.style.display = 'none';
+    }
+}
+
+//Funcao anonima para rolar até o topo
 document.querySelector('#arrow').addEventListener('click', function() {
-    window.scrollTo({top: 0, behavior: 'smooth'})
+    window.scrollTo({top: 0, behavior: 'smooth'});
 })
+
+//escutar o evento scroll
+window.addEventListener('scroll', buttomTop);
+
